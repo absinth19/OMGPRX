1fb28cdd18e2c144536185a951535562fea66e58
absinth19 <30318340+absinth19@users.noreply.github.com>
2025-05-13 16:10:08 +0200

    Update fullita.py

--- fullita.py
+++ fullita.py
@@ -13,7 +13,7 @@ import time
 # Constants
 
 LINKMFP = "https://trimming-passaggio.hf.space"
-PASSWORD = "ApiPassword"
+PASSWORD = "Leggila"
 
 PROXY = f"{LINKMFP}/proxy/hls/manifest.m3u8?api_password={PASSWORD}&d="
 
