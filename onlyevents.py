fdb1122f2d58a469d1fe7913a5248b7f0822f95d
absinth19 <30318340+absinth19@users.noreply.github.com>
2025-05-13 11:31:08 +0200

    Update onlyevents.py

--- onlyevents.py
+++ onlyevents.py
@@ -10,7 +10,7 @@ from bs4 import BeautifulSoup
 import time
 # Costanti
 
-LINKMFP = "https://trimming-mfp.hf.space"
+LINKMFP = "https://trimming-passaggio.hf.space"
 PASSWORD = "ApiPassword"
 
 PROXY = f"{LINKMFP}/proxy/hls/manifest.m3u8?api_password={PASSWORD}&d="
